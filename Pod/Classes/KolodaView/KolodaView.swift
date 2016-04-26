23dcfc3e86e9015cacf7d7895eccc1776c4d82a5
Lukas Zielinski <lukas.zielinski@otto.de>
2016-04-26 16:26:15 +0200

    For background cards change top margin to 0 and scale percent to 1.

--- Pod/Classes/KolodaView/KolodaView.swift
+++ Pod/Classes/KolodaView/KolodaView.swift
@@ -17,8 +17,8 @@ public enum SwipeResultDirection {
 
 //Default values
 private let defaultCountOfVisibleCards = 3
-private let backgroundCardsTopMargin: CGFloat = 4.0
-private let backgroundCardsScalePercent: CGFloat = 0.95
+private let backgroundCardsTopMargin: CGFloat = 0
+private let backgroundCardsScalePercent: CGFloat = 1
 private let backgroundCardsLeftMargin: CGFloat = 8.0
 private let backgroundCardFrameAnimationDuration: NSTimeInterval = 0.2
 
